a73ff5f0146f1f1fa6c7a2487dd6f684f5236275
kamaljeet singh rana <rmekamal789@gmail.com>
2022-11-14 18:13:40 +0530

    fisrt commit

--- moonj/registration/models.py
+++ moonj/registration/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class registration(models.Model):
+    name = models.CharField(max_length=100, verbose_name="name")
+    email = models.EmailField(max_length=254,verbose_name="email")
+    phone = models.CharField(max_length=50,verbose_name="phone")
+    address = models.TextField(null=True)
+    city = models.CharField(max_length=50 ,null=True)
+    pincode = models.IntegerField(null=True)
+class login(models.Model):
+    email = models.ForeignKey(registration,on_delete=models.CASCADE)
+    password = models.CharField(max_length=50)
