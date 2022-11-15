a053c89e87135342f82a2562249f9d7b448389f0
kamaljeet singh rana <rmekamal789@gmail.com>
2022-11-15 23:03:53 +0530

    backend

--- moonj/cart/views.py
+++ moonj/cart/views.py
@@ -3,42 +3,65 @@ from django.http import HttpResponse
 from .models import product,cart
 from registration.models import registration,login,Address
 from registration.views import login1
+from datetime import datetime
 # Create your views here.
+
+
 class dash_product():
-    def __init__(self,product_name,product_id,price,quantity):
+    def __init__(self,product_name,product_id,price,quantity,img):
         self.product_name = product_name
         self.product_id = product_id
         self.price = price
         self.quantity =quantity
+        self.img=img
     
 
 class cart_object():
-    def __init__(self,product_name,product_id,price,quantity):
+    def __init__(self,product_name,product_id,price,quantity,img):
         self.product_name = product_name
         self.product_id = product_id
         self.price = price
         self.quantity =quantity
+        self.img = img
+
+
 def dashboard(request):
     object_list = []
+    # if request.session.has_key('user_login_uesr_id'):
+    #     return render
+     #return render
+    if request.GET.get('pk')=="2":
+        try:
+            data = product.objects.all()[:9]
+        except:
+            print("not loaded")
+        else:
+            for x in data:
+                ob = dash_product(x.product_name,x.product_id,x.price,x.quantity,x.image)
+                print(ob.img.url)
+                object_list.append(ob)
+        context ={
+        "data":object_list
+        }
+        return render(request,'registeration/product.html',context)
     try:
         data = product.objects.all()[:3]
     except:
         print("not loaded")
     else:
         for x in data:
-            ob = dash_product(x.product_name,x.product_id,x.price,x.quantity)
-            #print(x.category_id.category_name)
+            ob = dash_product(x.product_name,x.product_id,x.price,x.quantity,x.image)
+            print(ob.img.url)
             object_list.append(ob)
     context ={
         "data":object_list
     }
-    # if request.session.has_key('user_login_uesr_id'):
-    #     return render
-     #return render
-    return HttpResponse("hello")
+    return render(request,'registeration/product.html',context)
+
+
 def cart1(request):
     object_list = []
-
+    
     if request.session.has_key('user_login_uesr_id') and request.POST.get("show_cart"):
         user_id = request.session['user_login_user_id']
         data = registration.objects.get(email= user_id)
@@ -48,8 +71,9 @@ def cart1(request):
         for x in data1:
 
             prod = product.objects.get(product_id = x.product_id.product_id)
-            #print(prod.product_name)
-            ob = cart_object(prod.product_name,prod.product_id,prod.price,x.quantity)
+            print(prod.product_name)
+            ob = cart_object(prod.product_name,prod.product_id,prod.price,x.quantity,x.image)
+            print(prod.image)           
             object_list.append(ob)
         context ={
             "data":object_list
@@ -57,13 +81,27 @@ def cart1(request):
         #print(object_list)
         return render()
     return render()
+
+
 def add_to_cart(request):
-    if request.session.has_key('user_login_uesr_id') and request.GET.get("add_to_cart"):
+    print(request.session['user_login_user_id'])
+    print(request.GET.get('productid'))
+    if request.session.has_key('user_login_user_id') and request.GET.get('productid')!="":
+        print("hello")
         quantity = 1
-        product_id = request.GET.get('product_id')
+        objtime = datetime.now().time()
+        product_id = request.GET.get('productid')
+        print(product_id)
+        product_obj = product.objects.get(product_id=product_id)
+
         user_id = request.session['user_login_user_id']
-        data = cart(email = user_id,product_id=product_id,quantity=quantity)
+        user_obj = registration.objects.get(email=user_id)
+
+        data = cart(email = user_obj,product_id=product_obj,quantity=quantity,time=objtime)
         data.save()
+    return HttpResponse("hello")
+
+
 def remove_from_cart(request):
     if request.session.has_key('user_login_user_id') and request.GET.get("remove_from_cart"):
         user_id= request.session['user_login_user_id']
@@ -72,14 +110,14 @@ def remove_from_cart(request):
         data_obj.delete()
     return redirect('login/')
 def checkout(request):
-    #if request.session.has_key('user_login_user_id') and request.GET.get("checkout"):
-        user_id = "rmekamal789@gmail.com"#request.session.has_key('user_login_user_id')
+    if request.session.has_key('user_login_user_id') and request.GET.get("checkout"):
+        user_id = request.session['user_login_user_id']
         data = registration.objects.get(email =user_id)
         cart_data = cart.objects.filter(email= data)
         total_price = 0
         for x in cart_data:
            product_id = x.product_id.product_id;
            price = int(product.objects.get(product_id=product_id).price)
-           total_price= total_price+price
+           total_price= total_price+(price*x.quantity)
         return HttpResponse(total_price)
 
